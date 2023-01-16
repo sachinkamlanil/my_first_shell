b711a72d452aad8194f59faf03171029f814ddd6
Ashish <122161380+ashusanvatsarkar@users.noreply.github.com>
2023-01-16 19:26:47 +0530

    my assignment

---
+++ ashish_assignment.sh
@@ -0,0 +1,23 @@
+echo -e "Enter First Number : \c"
+read first_num
+echo -e "Enter Second Number : \c"
+read second_num
+echo -e "Enter Operator : \c"
+read opt
+
+if [ $opt = 'add' ]
+then
+    echo $( expr $first_num + $second_num )
+elif [ $opt = 'sub' ]
+then
+    echo $( expr $first_num - $second_num )
+elif [ $opt = 'mul' ]
+then
+    echo $( expr $first_num \* $second_num )
+elif [ $opt = 'div' ]
+then
+    echo $( expr $first_num / $second_num )
+elif [ $opt = 'rmd' ]
+then
+    echo $( expr $first_num % $second_num )
+fi
